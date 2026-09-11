ae5f023579ae41c73710fde5e94b6635a48635c2
ContrerasMarco <mcontreras42@outlook.com>
2026-09-11 12:17:10 -0300

    recorriendo lista con for e imprimiendo cada elemento

---
+++ recorriendo_lista_con_for.py
@@ -0,0 +1,12 @@
+#Ejercicio para la Sección 4.2 🏆
+
+#Imagina que tienes la lista de las compras de la semana:
+
+#compras = ["pan", "leche", "huevos", "café"]
+#Escribe un programa utilizando un bucle for que recorra la lista compras 
+# e imprima para cada elemento el siguiente mensaje: "Tengo que comprar: [nombre del producto]"
+
+compras = ["pan", "leche", "huevos", "cafe"]
+
+for productos in compras:
+    print("Tengo que comprar : ",productos )
